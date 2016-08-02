30e2f39bedf520df719a1048dc1ed6bdcdace582
LemondRinds <diveaway89@gmail.com>
2016-08-02 12:47:02 -0400

    footer rework

--- footer.php
+++ footer.php
@@ -1,32 +1,80 @@
 			<div id="footer">
-
-				<div id="footer-navwrap" role="navigation" class="screen-only">
+				
+				<div class="collapse col">
 					<?=wp_nav_menu(array(
-						'theme_location' => 'footer-menu',
+						//'theme_location' => 'footer-menu',
 						'container' => 'false',
-						'menu_class' => 'menu list-unstyled list-inline text-center',
-						'menu_id' => 'footer-menu',
+						'menu_class' => 'menu list-unstyled text-left',
+						'menu_id' => 'audience',
+						'fallback_cb' => false,
+						'depth' => 1,
+						'walker' => new Bootstrap_Walker_Nav_Menu()
+						));
+					?>				
+				</div>
+				<div class="collapse col">
+					<?=wp_nav_menu(array(
+						//'theme_location' => 'footer-menu',
+						'container' => 'false',
+						'menu_class' => 'menu list-unstyled text-left',
+						'menu_id' => 'quick-links',
 						'fallback_cb' => false,
 						'depth' => 1,
 						'walker' => new Bootstrap_Walker_Nav_Menu()
 						));
 					?>
 				</div>
-				<div id="footer-logo">
-					<a href="http://tbhccmsdev.smca.ucf.edu">
-						<img id="tbhcFooterLogo" src="<?php bloginfo('stylesheet_directory'); ?>/static/img/WebsiteFooterShort.png" alt="The Burnett Honors College">
-						</img>
-					</a>
+				<div id="footerLogo">
+						<a href="http://tbhccmsdev.smca.ucf.edu">
+							<img id="tbhcFooterLogo" src="<?php bloginfo('stylesheet_directory'); ?>/static/img/WebsiteFooterShort.png" alt="The Burnett Honors College">
+							</img>
+						</a>
+					<?=wp_nav_menu(array(
+						//'theme_location' => 'social-links',
+						'container' => 'div',
+						'container_id' => 'social-menu-wrap',
+						'menu_class' => 'menu list-unstyled text-left screen-only',
+						'menu_id' => 'social-menu',
+						'depth' => 1,
+						));
+					?>
 				</div>
-				<?=wp_nav_menu(array(
-					'theme_location' => 'social-links',
-					'container' => 'div',
-					'container_id' => 'social-menu-wrap',
-					'menu_class' => 'menu list-unstyled list-inline text-center screen-only',
-					'menu_id' => 'social-menu',
-					'depth' => 1,
-					));
-				?>
+				<div class="col">
+					<?=wp_nav_menu(array(
+						//'theme_location' => 'department-sites',
+						'container' => 'div',
+						'menu_class' => 'menu list-unstyled text-left screen-only',
+						'menu_id' => 'department-sites',
+						'depth' => 1,
+						));
+					?>
+				</div>
+				<div class="col">
+					<p id="" role="contentinfo" class="vcard">
+						<div class="adr">
+							<div class="street-address">P.O. Box 161800 </div>
+							<div class="locality">Orlando</div>,
+							<div class="region">Florida</div>,
+							<div class="postal-code">32816-1800</div> |
+							<div class="tel"><a href="tel:4078232076">407.823.2076</a></div>
+						</div>
+						<!--<br/>
+						<a href="<?=site_url()?>/feedback/">Comments and Feedback</a> | &copy;
+						<a href="<?=site_url()?>" class="print-noexpand fn org url">
+							<span class="organization-name">The Burnett Honors College</span>
+						</a>-->
+					</p>
+				</div>
+			
+			
+			
+			
+			
+			
+<!--				<div id="footer-navwrap" role="navigation" class="screen-only">
+					
+				</div>
+
 
 				<p id="subfooter" role="contentinfo" class="vcard">
 					<span class="adr">
@@ -41,7 +89,7 @@
 					<a href="<?=site_url()?>" class="print-noexpand fn org url">
 						<span class="organization-name">The Burnett Honors College</span>
 					</a>
-				</p>
+				</p>-->
 
 			</div>
 		</div><!-- .container -->
