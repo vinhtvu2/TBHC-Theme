24bf61f2f9c49c68f5058b17274cfdd6a6291388
LemondRinds <diveaway89@gmail.com>
2016-08-03 13:50:55 -0400

    maybe switching footer to grid instead of table

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
 			<div id="footer">
-				<div id="footer-navwrap" class="row">
-					<div class="col-xs-2 hidden-sm footerCol">
+				<div id="footer-navwrap">
+					<div class="hidden-sm footerCol">
 						<?=wp_nav_menu(array(
 							//'theme_location' => 'footer-menu',
 							'menu' => 31,
 							'container' => 'false',
-							'menu_class' => 'menu list-unstyled',
+							'menu_class' => 'menu list-unstyled text-right',
 							'fallback_cb' => false,
 							'depth' => 1,
 							'walker' => new Bootstrap_Walker_Nav_Menu()
 							));
 						?>				
 					</div>
-					<div class="col-xs-2 hidden-sm footerCol">
+					<div class="hidden-sm footerCol">
 						<?=wp_nav_menu(array(
 							//'theme_location' => 'footer-menu',
 							'menu' => 6,
 							'container' => 'false',
-							'menu_class' => 'menu list-unstyled',
+							'menu_class' => 'menu list-unstyled text-right',
 							//'menu_id' => 'quick-links',
 							'fallback_cb' => false,
 							'depth' => 1,
@@ -25,18 +25,18 @@
 							));
 						?>
 					</div>
-					<div class="col-xs-4 col-md-2 col-md-push-2 footerCol">
+					<div class="footerCol">
 						<?=wp_nav_menu(array(
 							//'theme_location' => 'department-sites',
 							'menu' => 7,
 							'container' => 'div',
-							'menu_class' => 'menu list-unstyled screen-only',
+							'menu_class' => 'menu list-unstyled text-left screen-only',
 							//'menu_id' => 'department-sites',
 							'depth' => 1,
 							));
 						?>
 					</div>
-					<div id="footer-logo" class="col-xs-4 col-md-2 col-md-pull-2 footerCol">
+					<div id="footer-logo" class="footerCol ">
 						<a href="http://tbhccmsdev.smca.ucf.edu">
 							<img id="tbhcFooterLogo" src="<?php bloginfo('stylesheet_directory'); ?>/static/img/WebsiteFooterShort.png" alt="The Burnett Honors College">
 							</img>
@@ -50,8 +50,8 @@
 							'depth' => 1,
 							));
 						?>
-					</div>
-					<div class="col-xs-4 col-md-2 footerCol">
+					</div>					
+					<div class="footerCol">
 						<p id="contactInfo" role="contentinfo" class="vcard">
 							<a href="<?=site_url()?>" class="print-noexpand fn org url">
 								<span class="organization-name">The Burnett Honors College</span>
