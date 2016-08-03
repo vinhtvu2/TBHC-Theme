cc1a911bedc0806054f3c6a79041ef7da8f46cfd
LemondRinds <diveaway89@gmail.com>
2016-08-03 13:28:19 -0400

    maybe switching footer to grid instead of table

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 			<div id="footer">
 				<div id="footer-navwrap" class="row">
-					<div class="col-xs-2 xs-hidden">
+					<div class="col-xs-2 hidden-xs">
 						<?=wp_nav_menu(array(
 							//'theme_location' => 'footer-menu',
 							'menu' => 31,
@@ -12,7 +12,7 @@
 							));
 						?>				
 					</div>
-					<div class="col-xs-2 xs-hidden">
+					<div class="col-xs-2 hidden-xs">
 						<?=wp_nav_menu(array(
 							//'theme_location' => 'footer-menu',
 							'menu' => 6,
