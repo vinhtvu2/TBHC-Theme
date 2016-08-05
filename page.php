29bd7274ec45e67edb57d2e6662c7dff1bf42c25
LemondRinds <diveaway89@gmail.com>
2016-08-05 16:00:41 -0400

    default page template rework

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php get_header(); the_post();?>
 	<div class="row page-content" id="<?php echo $post->post_name; ?>">
-		<div class="col-md-12 col-sm-12">
+		<div class="col-md-15 col-sm-15">
 			<div id="page-title">
 				<div class="row">
-					<div class="col-md-9 col-sm-9">
+					<div class="col-md-12 col-sm-12">
 						<h1><?php the_title(); ?></h1>
 					</div>
 					<?php esi_include( 'output_weather_data', 'col-md-3 col-sm-3' ); ?>
@@ -13,14 +13,14 @@
 
 		<?=get_page_subheader($post)?>
 
-		<div class="col-md-7 col-sm-7 col-md-push-2 col-sm-push-2" id="contentcol">
+		<div class="col-sm-12 col-sm-push-3" id="contentcol">
 			<article role="main">
 				<?php the_content();?>
 				&nbsp;
 			</article>
 		</div>
 
-		<div id="sidebar_left" class="col-md-2 col-sm-2 col-md-pull-7 col-sm-pull-7" role="navigation">
+		<div id="sidebar_left" class="col-sm-3 col-sm-pull-12" role="navigation">
 			<?=get_sidebar('left');?>
 		</div>
 		
