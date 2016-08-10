76ed22f8125c54f40ffd2b68c0813e7174cf8304
LemondRinds <diveaway89@gmail.com>
2016-08-10 16:01:32 -0400

    changed events links

--- functions/feeds.php
+++ functions/feeds.php
@@ -204,7 +204,7 @@ function display_events($start=null, $limit=null){?>
 					$start 		= new DateTime($item['starts']);
 					$day 		= $start->format('M d');
 					$time 		= $start->format('h:i a');
-					$link 		= $url.'eventdatetime_id='.$item['id']; // TODO: use 'url' after unify-events launches
+					$link 		= 'https://events.ucf.edu/event/'.$item['id'].'/'.$item['title'];//$url.'eventdatetime_id='.$item['id']; // TODO: use 'url' after unify-events launches
 					$loc_link 	= $item['location_url'];
 					$location	= $item['location'];
 					$title		= $item['title'];
