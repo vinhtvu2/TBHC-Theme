681ee75a20bed01e2bec1dc8bdd8420bb47963ab
LemondRinds <diveaway89@gmail.com>
2016-08-11 11:50:07 -0400

    staff thumb size++, display org_group on person grid

--- single-person.php
+++ single-person.php
@@ -15,7 +15,7 @@
 		<div class="col-md-3 col-sm-3 details">
 			<?
 				$title = get_post_meta($post->ID, 'person_jobtitle', True);
-				$image_url = get_featured_image_url($post->ID);
+				$image_url = get_featured_image_url($post->ID, 'person-grid-image');
 				$email = get_post_meta($post->ID, 'person_email', True);
 				$phones = Person::get_phones($post);
 			?>
