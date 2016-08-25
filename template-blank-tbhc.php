1b3020240dbbab2f173d1d0878d6819c97ac9641
LemondRinds <diveaway89@gmail.com>
2016-08-25 14:32:25 -0400

    tryin out ajax stuffff

--- template-blank-tbhc.php
+++ template-blank-tbhc.php
@@ -4,11 +4,6 @@
  **/
 ?>
 <?php get_header(); the_post();?>
-<?php 
-	if(is_page() && $post = get_page_by_title( 'staff-profiles')){
-		
-	}
-	the_content();
-?>
+<?php the_content();?>
 </div><!--.container [hidden rage]-->
 <?php get_footer();?>
