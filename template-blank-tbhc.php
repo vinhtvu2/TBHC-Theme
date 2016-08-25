2728dcef511943700c9ac28e14de0f846d0e97a7
LemondRinds <diveaway89@gmail.com>
2016-08-25 14:41:07 -0400

    tryin out ajax stuffff

--- template-blank-tbhc.php
+++ template-blank-tbhc.php
@@ -4,6 +4,12 @@
  **/
 ?>
 <?php get_header(); the_post();?>
-<?php the_content();?>
+<?php 
+	if(is_page() && get_the_title() == 'staff-profiles'){
+		remove_filter( 'the_content', 'wpautop' );
+		add_filter( 'the_content', 'wpautop' , 99);
+	}
+	the_content();
+?>
 </div><!--.container [hidden rage]-->
 <?php get_footer();?>
