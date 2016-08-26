b97f397731bf2a507259a683963205e372abea19
LemondRinds <diveaway89@gmail.com>
2016-08-26 14:26:06 -0400

    tweak the shortcode for profile grid

--- shortcodes.php
+++ shortcodes.php
@@ -296,7 +296,7 @@ function sc_person_profile_grid($atts) {
 						'class'	=>	'person-profile-grid-dropdown form-control',
 						'echo'	=> false,
 						'selected'	=>	$org_groups,
-						'child_of'	=>	(int)get_term_by('slug', 'Staff', 'org_groups')->term_id,
+						'child_of'	=>	$org_groups == 'staff' ? (int)get_term_by('slug', 'Staff', 'org_groups')->term_id : NULL,
 					)
 				)
 			);
