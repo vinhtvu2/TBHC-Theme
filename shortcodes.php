70d087d501e2abd8c3414d01ef532b806b7010e6
LemondRinds <diveaway89@gmail.com>
2016-08-30 10:37:07 -0400

    profile grid dd stuff

--- shortcodes.php
+++ shortcodes.php
@@ -307,13 +307,13 @@ function sc_person_profile_grid($atts) {
 		$count = 0;
 		foreach($people as $person) {
 			
-			$OGID = get_term_by('name', $dd_org_groups, 'org_groups')->term_id;
+			$OGID = get_term_by('name', $dd_org_groups, 'org_groups');
 			$term_list = wp_get_post_terms($person->ID, 'org_groups');
 			$terms = array_filter($term_list, function($obj) {
 					print_r("parent".$obj->parent);
 					print_r($OGID);
 					print_r("equiv".($obj->parent == 11));
-				return $obj->parent == $OGID;//->term_id;
+				return $obj->parent == $OGID->term_id;
 			});
 			$terms = implode(", ", array_map(function($obj){
 				return $obj->name;
