3d3ce739e68f056f2553cd48ddf7ef1242a4ed23
LemondRinds <diveaway89@gmail.com>
2016-08-30 13:30:31 -0400

    profile grid dd stuff

--- shortcodes.php
+++ shortcodes.php
@@ -270,7 +270,7 @@ function sc_person_profile_grid($atts) {
 	$dropdown		= ($atts['dropdown']) ? $atts['dropdown'] : false;
 	$dd_org_groups	= ($atts['dd_org_groups']) ? $atts['dd_org_groups'] : $org_groups;
 	$show_org_group	= ($atts['show_org_group']) ? $atts['show_org_group'] : false;
-	$OGKids 		= get_term_children(get_term_by('slug', $dd_org_groups, 'org_groups'), 'org_groups');	
+	$OGKids 		= get_term_children(get_term_by('name', $dd_org_groups, 'org_groups'), 'org_groups');	
 	$people 		= sc_object_list(
 	array(
 	'type' => 'person',
