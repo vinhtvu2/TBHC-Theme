1443f008eefd34d49d3aa5e3a46b69e2f1e8378c
LemondRinds <diveaway89@gmail.com>
2016-08-31 11:02:54 -0400

    profile grid dd stuff

--- src/js/script.js
+++ src/js/script.js
@@ -1597,12 +1597,13 @@ function getProfilesForGrid(inp1, inp2){
 	var ajaxUrl = $(".person-profile-grid").data("url");
 	var reqGrp = $(".person-profile-grid").data("group");
 	var regGrp2 = $(".person-profile-grid").data("group2");
+	regGrp2 = typeof regGrp2 === 'undefined' || !regGrp2 ? regGrp2 : null; 
 	var shwOrgs = $(".person-profile-grid").data("shwGrp");
 	jQuery.post(ajaxUrl, {
 		action : 'get_people_from_org_group',
 		org_groups: inp1,
 		dd_org_groups: reqGrp,
-		dd2_org_groups: reqGrp2 ? regGrp2 : null,
+		dd2_org_groups: reqGrp2,
 		show_org_groups: shwOrgs
 	}, function(res){
 		jQuery('.person-profile-grid').replaceWith(res);
