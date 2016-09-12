20b6c4138a978554b8ddb4bf3e17fae17877314d
LemondRinds <diveaway89@gmail.com>
2016-09-12 16:41:08 -0400

    js and ajax for dropdown panel

--- functions.php
+++ functions.php
@@ -690,8 +690,6 @@ function frontpage_opportunities() {
 				$thumb_src = $thumb_src[0];
 			?>
 			<?php if ($thumb_src) { ?>
-				<?=print_r(get_post_meta($opportunity->ID, 'opportunity_start'))?>				
-				<?=print_r(get_post_meta($opportunity->ID, 'opportunity_end'))?>
 				<img class="print-only opportunity_thumb" src="<?=esc_attr($thumb_src)?>" alt="<?=esc_attr($spotlight->post_title)?>"/>
 				<div class="screen-only opportunity_thumb" style="background-image:url('<?=esc_attr($thumb_src)?>');"><?=esc_attr($opportunity->post_title)?></div>
 			<?php } ?>
