0c2dacf487c4d61154ec5b15e23618a0cd5b8ab6
LemondRinds <diveaway89@gmail.com>
2016-09-12 11:54:57 -0400

    js and ajax for dropdown panel

--- functions.php
+++ functions.php
@@ -631,9 +631,6 @@ function frontpage_opportunities() {
 	$args = array(
 		'post_type' 	=> 'opportunity',
 		'post_status'   => 'publish',
-		'meta_key'      => 'opportunity_end',
-		'orderby' 		=> 'meta_value',
-		'order'			=> 'DESC',
 		'meta_query'	=> array(
 			array(
 				'key'	=>	'opportunity_post_to_home',
