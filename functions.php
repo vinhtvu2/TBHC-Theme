579f305eba21da3fa08a44bd8bfdbf4ac387c69d
LemondRinds <diveaway89@gmail.com>
2016-09-23 10:33:13 -0400

    switched filter and render on post_content to get_post w/ ID in nav panel pull

--- functions.php
+++ functions.php
@@ -1987,7 +1987,7 @@ function get_nav_panel(){
 	);
 	$items = get_posts($args);
 	if(is_array($items) && !empty($items)){
-		print_r(apply_filters('the_content', $items[0]->post_content));
+		print_r(get_post($items[0]->ID));
 	}else{
 		print_r("Please check back later! This panel is under maintenance!");
 	}
