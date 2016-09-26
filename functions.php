6ad804e212a7867383cafd6612769dc3088fea62
LemondRinds <diveaway89@gmail.com>
2016-09-26 15:26:50 -0400

    strcmp add to compare fnxn causes probs, removing for debug

--- functions.php
+++ functions.php
@@ -633,8 +633,8 @@ function frontpage_opportunities() {
 		'post_type' 	=> 'opportunity',
 		'post_status'   => 'publish',
 		'meta_key'		=> 'opportunity_end',
-		'orderby'		=> 'meta_value_num',
-		'order'			=> 'DESC',
+		//'orderby'		=> 'meta_value_num',
+		//'order'			=> 'DESC',
 		'meta_query'	=> array(
 			/*array(
 				'key'	=>	'opportunity_post_to_home',
@@ -660,8 +660,8 @@ function frontpage_opportunities() {
 		'post_type' 	=> 'opportunity',
 		'post_status'   => 'publish',
 		'meta_key'		=> 'opportunity_end',
-		'orderby'		=> 'meta_value_num',
-		'order'			=> 'DESC',		
+		//'orderby'		=> 'meta_value_num',
+		//'order'			=> 'DESC',		
 		);
 		$opportunities = get_posts($args);
 	}
