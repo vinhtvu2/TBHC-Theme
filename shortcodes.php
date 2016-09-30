bd8a6c81d24a4c813aa88fda3fa44bf43dfc31e8
LemondRinds <diveaway89@gmail.com>
2016-09-30 10:23:18 -0400

    added usort to opp archive

--- shortcodes.php
+++ shortcodes.php
@@ -431,6 +431,21 @@ function sc_opportunity_grid($atts) {
 		'objects_only' => True,
 	));
 	
+	$dt = new DateTime();	
+	usort($opps, function($a, $b)use($dt){
+		$a_dt = new DateTime(get_post_meta($a->ID, 'opportunity_end', TRUE));
+		$b_dt = new DateTime(get_post_meta($b->ID, 'opportunity_end', TRUE));
+		$a_diff = $a_dt->diff($dt);
+		$b_diff = $b_dt->diff($dt);
+		if ($a_diff->days == $b_diff->days) {
+			// If they have the same depth, compare titles
+			return strcmp($a->post_title, $b->post_title);
+		}
+		// If depth_a is smaller than depth_b, return -1; otherwise return 1
+		$res = ($a_diff->days < $b_diff->days) ? -1 : 1;
+		return $res;
+	});
+	
 	ob_start();
 	?><div class="opportunity-grid" data-url="<?=admin_url( 'admin-ajax.php' )?>" data-group="<?=$dd_event_groups?>" data-group2="<?=$dd2_event_groups?>" data-jn="<?=$join?>" data-oprtr="<?=$operator?>" data-allopt="<?=$show_option_all?>" data-allopt2="<?=$show_option_all2?>">
 		<? if($dropdown){ 
