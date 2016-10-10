1b1be02f63328bf82aed86e52f8e2753285afd1f
LemondRinds <diveaway89@gmail.com>
2016-10-10 14:34:02 -0400

    replaced 'r' (right) with 'l' (left) in the sidebar-left.php fnxn var gets

--- sidebar-left.php
+++ sidebar-left.php
@@ -64,8 +64,8 @@
 		print '</div>';
 	}	
 	// Embed Widget 1
-	$embed1_title	 		= get_post_meta($post->ID, 'page_widget_r_embed1_title', TRUE);
-	$embed1			 		= get_post_meta($post->ID, 'page_widget_r_embed1', TRUE);	
+	$embed1_title	 		= get_post_meta($post->ID, 'page_widget_l_embed1_title', TRUE);
+	$embed1			 		= get_post_meta($post->ID, 'page_widget_l_embed1', TRUE);	
 	if ($embed1) {	
 		if ($embed1_title !== '') {
 			print '<h3 id="sidebar_l_embed1" class="sidebar_title">'.$embed1_title.'</h3>';
