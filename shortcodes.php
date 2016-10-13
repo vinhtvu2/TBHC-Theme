0caa9ca0cd858f2123768d54d1600e0a55283943
LemondRinds <diveaway89@gmail.com>
2016-10-13 10:56:21 -0400

    more debugging stuff

--- shortcodes.php
+++ shortcodes.php
@@ -563,9 +563,7 @@ function sc_opportunity_grid($atts) {
 					//var_dump($matches);					
 					//$cPost = $matches['url'];					
 					//var_dump($cPost);
-					$cPost = url_to_postid($cPost);
-					var_dump($cPost);					
-					$cPost = get_post($cPost);
+					$cPost = get_page_by_path($cPost);
 					var_dump($cPost);					
 					$cPost = wp_trim_words($cPost->post_content, 200);
 					var_dump($cPost);
