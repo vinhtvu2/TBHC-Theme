bbd120f47194feca2f604498aaa598d2f9beb68d
LemondRinds <diveaway89@gmail.com>
2016-10-13 11:12:59 -0400

    more debugging stuff

--- shortcodes.php
+++ shortcodes.php
@@ -557,14 +557,17 @@ function sc_opportunity_grid($atts) {
 				foreach ($opps as $opportunity) { 
 					$start_date = get_post_meta($opportunity->ID, 'opportunity_start', TRUE);
 					$end_date = get_post_meta($opportunity->ID, 'opportunity_end', TRUE);
-					$cPost = get_post_meta($opportunity->ID, 'opportunity_url_redirect', true);
-					var_dump($cPost);
+					//$cPost = get_post_meta($opportunity->ID, 'opportunity_url_redirect', true);
+					//var_dump($cPost);
 					//preg_match('/(?:http|https):\/\/tbhccmsdev.smca.ucf.edu\/(?<url>\S*)(?:\/*)/', $cPost, $matches);
 					//var_dump($matches);					
 					//$cPost = $matches['url'];					
 					//var_dump($cPost);
-					$cPost = get_page_by_path(basename(untrailingslashit($cPost)));
-					var_dump($cPost);					
+					//$cPost = url_to_postid($cPost);
+					//var_dump($cPost);					
+					//$cPost = get_post($cPost);
+					//var_dump($cPost);		
+					$cPost = get_page_by_title($opportunity->page_title, OBJECT, 'post');
 					$cPost = wp_trim_words($cPost->post_content, 200);
 					var_dump($cPost);
 					$time = '';
