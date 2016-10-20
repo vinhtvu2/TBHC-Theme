c44b56ab6683c420a70ee508910ad2af9c0b24d4
LemondRinds <diveaway89@gmail.com>
2016-10-20 15:22:59 -0400

    testing custom var in bootstrap nav walker, intend to remove ajax part of nav dropdowns

--- functions/base.php
+++ functions/base.php
@@ -581,7 +581,7 @@ function bootstrap_menus() {
 
 			function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
 				
-				var_dump($args[0]['nav_dropdowns']);					
+				var_dump($args[0][0]);					
 				
 				if ( !$element )
 					return;
