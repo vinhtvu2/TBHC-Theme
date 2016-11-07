3e5e6a72f1400485c99eebcc0282a5f72ddbe120
LemondRinds <diveaway89@gmail.com>
2016-11-07 09:11:18 -0500

    big gold bottom hugging 'moar' buttons in homepage cols

--- home.php
+++ home.php
@@ -5,12 +5,12 @@
 		<div class="col-sm-5 col-md-xpad col-sm-xpad" id="home_leftcol">
 			<h2>Spotlight</h2>
 			<?=frontpage_spotlights()?>
-			<p class="screen-only"><a href="<?=get_permalink(get_page_by_title('Spotlight Archives', OBJECT, 'page')->ID);?>" class="home_col_morelink">Spotlight Archive</a></p>
+			<p class="screen-only moreBtn"><a href="<?=get_permalink(get_page_by_title('Spotlight Archives', OBJECT, 'page')->ID);?>" class="home_col_morelink">Spotlight Archive</a></p>
 		</div>
 		<div class="col-sm-5 col-md-xpad col-sm-xpad" id="home_centercol">
 			<h2>Opportunities</h2>
 			<?=frontpage_opportunities()?>
-			<p class="screen-only"><a href="<?=get_permalink(get_page_by_title('Opportunities', OBJECT, 'page')->ID);?>" class="home_col_morelink">More Opportunities</a></p>
+			<p class="screen-only moreBtn"><a href="<?=get_permalink(get_page_by_title('Opportunities', OBJECT, 'page')->ID);?>" class="home_col_morelink">More Opportunities</a></p>
 		</div>
 		<div class="col-sm-5 col-md-xpad col-sm-xpad" id="home_rightcol">
 			<h2>Upcoming Events</h2>
