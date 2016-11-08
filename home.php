04e57389501e410884d550843c1fe8b49998ab7f
LemondRinds <diveaway89@gmail.com>
2016-11-08 10:42:39 -0500

    forgot to replace br with hr

--- home.php
+++ home.php
@@ -3,17 +3,17 @@
 <?php $page    = get_page_by_title('Home');?>
 	<div class="row" id="home" data-template="home-nodescription" role="main">
 		<div class="col-sm-5 col-md-xpad col-sm-xpad" id="home_leftcol">
-			<h2>Spotlight<br/></h2>
+			<h2>Spotlight<hr/></h2>
 			<?=frontpage_spotlights()?>
 			<p class="screen-only moreBtn"><a href="<?=get_permalink(get_page_by_title('Spotlight Archives', OBJECT, 'page')->ID);?>" class="home_col_morelink">Spotlight Archive</a></p>
 		</div>
 		<div class="col-sm-5 col-md-xpad col-sm-xpad" id="home_centercol">
-			<h2>Opportunities<br/></h2>
+			<h2>Opportunities<hr/></h2>
 			<?=frontpage_opportunities()?>
 			<p class="screen-only moreBtn"><a href="<?=get_permalink(get_page_by_title('Opportunities', OBJECT, 'page')->ID);?>" class="home_col_morelink">More Opportunities</a></p>
 		</div>
 		<div class="col-sm-5 col-md-xpad col-sm-xpad" id="home_rightcol">
-			<h2>Upcoming Events<br/></h2>
+			<h2>Upcoming Events<hr/></h2>
 			<?php esi_include('do_shortcode','[events-widget]'); ?>
 			<?php esi_include('output_weather_data'); ?>
 		</div>
