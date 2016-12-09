1e70ae7f910f35b176e91662f85d8c22e24ddac5
LemondRinds <diveaway89@gmail.com>
2016-12-09 15:24:39 -0500

    ucf search header is apparently lazy loaded... 70px extra needed it seems

--- src/js/script.js
+++ src/js/script.js
@@ -1739,6 +1739,6 @@ var debounce = function (func, threshold, execAsap) {
 $(function() {
     $('#header-nav-wrap').height($("#header-nav-wrap").height());
     $('#header-nav-wrap').affix({
-        offset: { top: $('#header-nav-wrap').offset().top }
+        offset: { top: $('#header-nav-wrap').offset().top + 70 } // plus 70 for the danged ucf search bar header that loads late fml
     });
-});
+})();
