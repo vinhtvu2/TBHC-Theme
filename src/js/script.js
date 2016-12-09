503727e533cc6cb7f207ce0b85315c9549ab0ad0
LemondRinds <diveaway89@gmail.com>
2016-12-09 15:46:33 -0500

    sod it, messed somthn up with merging, manual fix

--- src/js/script.js
+++ src/js/script.js
@@ -1740,5 +1740,6 @@ $(function() {
     $('#header-nav-wrap').height($("#header-nav-wrap").height());
     $('#header-nav-wrap').affix({
         offset: { top: $('#header-nav-wrap').offset().top + 70 } // plus 70 for the danged ucf search bar header that loads late fml
+	});
 	$('#cntrPceWrap').on('affix-top.bs.affix', function(){ $(this).css('top','58px') })
 });
