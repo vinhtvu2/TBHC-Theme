7755800bab0afe870f3d5bd6fca4b673d09ca0a7
LemondRinds <diveaway89@gmail.com>
2017-01-24 10:58:08 -0500

    remov cors for debug

--- functions.php
+++ functions.php
@@ -13,10 +13,10 @@ require_once('third-party/truncate-html.php');  # Includes truncateHtml function
 // stuff for cors
 add_filter( 'allowed_http_origins', 'add_allowed_origins' );
 function add_allowed_origins( $origins ) {
-    $origins[] = site_url(null, null, 'http');
+    /*$origins[] = site_url(null, null, 'http');
     $origins[] = site_url(null, null, 'https');
 	print_r($origins);
-	echo $origins;
+	echo $origins;*/
     return $origins;
 }
 
