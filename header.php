5bc50746741e3f2ae67b0c74d9f9d46c064173d0
LemondRinds <diveaway89@gmail.com>
2017-02-13 13:24:11 -0500

    fixed theme option centerpiece height style, max-width changed to min

--- header.php
+++ header.php
@@ -82,7 +82,7 @@
 			#centerpiece_slider .centerpiece_single {
 				height: <?=get_theme_option('centerpiece_mobile_height')?>;
 			}
-			@media (min-width: $screen-xs-max) {
+			@media (min-width: 770px) {
 				#centerpiece_slider .centerpiece_single {
 					height: <?=get_theme_option('centerpiece_desktop_height')?>;
 				}
