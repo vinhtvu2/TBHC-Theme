eedae184f18a99787906b6315a0b10b3fd3311ba
LemondRinds <diveaway89@gmail.com>
2017-02-21 11:46:55 -0500

    added section menu back in

--- home.php
+++ home.php
@@ -1,29 +1,6 @@
 <?php get_header();?>
 <?php $options = get_option(THEME_OPTIONS_NAME);?>
 <?php $page    = get_page_by_title('Home');?>
-	<!--<nav id="section-nav-xs" class="visible-xs-block navbar navbar-inverse">
-		<div class="navbar-section">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#section-menu-xs-collapse" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<span class="navbar-brand">Navigation</span>
-		</div>
-		<div class="collapse navbar-collapse" id="section-menu-xs-collapse">
-			<?php
-				/*wp_nav_menu( array(
-				'theme_location' => 'homepage-sections',
-				'container' => false,
-				'menu_class' => 'menu nav navbar-nav',
-				'menu_id' => 'section-menu-xs',
-				'walker' => new Bootstrap_Walker_Nav_Menu(),
-				'nav_dropdowns'	=> false
-				) );*/
-			?>
-		</div>
-	</nav>-->
 	<div class="row" id="home" data-template="home-nodescription" role="main">
 		<?=frontpage_opportunities()?>
 		<?=frontpage_interests()?>
